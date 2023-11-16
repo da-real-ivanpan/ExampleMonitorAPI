b9a23f61ab8fe914129b261b13bb38e702dca394
Ivan Panasenko <carlsbergv@gmail.com>
2023-11-17 01:25:19 +0200

    Додано теки в гілці version3

---
+++ opt/test_service/myservice.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+start() {
+cd /opt/test_service/pub/
+python3 -m http.server 8090 > /dev/null 2> /dev/null &
+}
+stop() {
+kill -s SIGKILL `ps -ef | grep -i "http.server 8090" | awk '{print $2;}'`
+}
+case $1 in
+start|stop) "$1" ;;
+esac
+echo ""
